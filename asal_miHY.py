a6f9fedec05e61e5500aa62d1a708e34fb4f839c
seyhuni <huseyin.yanik@windowslive.com>
2021-02-17 12:40:53 +0300

    Update asal_miHY.py

--- asal_miHY.py
+++ asal_miHY.py
@@ -4,16 +4,3 @@ Created on Fri Sep 18 17:36:13 2020
 
 @author: asus
 """
-
-def asal_mi(sayi):
-    for i in range(2,sayi):
-        if(sayi%i == 0):
-            return False
-        return True
-        
-while True:
-    a = input("sayi giriniz:")
-    if a =="q":
-        break
-    else:
-        print(asal_mi(int(a)))
