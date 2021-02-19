e5cfc2b54d95bac1c7c339b96234a23ee12d84c3
Mustafa Berkan BİÇER <mberkanbicer@gmail.com>
2021-02-19 18:09:29 +0300

    last modifications

---
+++ GCCR003_aim1_MBB_19_02_2021.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import datetime as dt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn import metrics
+import numpy as np
+from sksurv.nonparametric import kaplan_meier_estimator
+
+# Separate classifications of “return to normal” will be created for taste and for smell.
+# We will a) perform survival analysis for chemosensory long haulers, using time in days between the date of onset of initial symptoms and the completion day of the follow-up survey.
+# We will also b) use linear regression to predict the difference score using the degree of loss in an initial survey and in a follow-up survey, controlling for time passed between the two surveys.
+
+data = pd.read_csv(r'C:/Users/bicers/gccr003_data.csv',sep=';',encoding='unicode_escape')
+
+data["Date_of_onset"] = pd.to_datetime(data["Date_of_onset"], dayfirst=True)
+data["Email_Time_Stamp_y"] = pd.to_datetime(data["Email_Time_Stamp_y"], dayfirst=True)
+
+data = data.dropna(subset=['Date_of_onset', 'Email_Time_Stamp_y'])
+data['DurationInDays'] = ((data['Email_Time_Stamp_y'] - data['Date_of_onset']).dt.total_seconds() / (60 * 60 * 24))
+
+data = data[data['Date_of_onset'].dt.year > 2018] # Include data after COVID
+
+data.info()
+data.describe()
+data.head()
+
+# create new dataframe variables
+smell_df = data[['DurationInDays', 'Smell_before_illness', 'Smell_current', 'Status']]
+taste_df = data[['DurationInDays', 'Taste_before_illness', 'Taste_current', 'Status']]
+
+#♣ drop nan records
+smell_df = smell_df.dropna()
+taste_df = taste_df.dropna()
+
+# Individuals who reported severe chemosensory dysfunction pre-COVID (i.e., before illness rating <50 for “before illness rating” on the original survey) will be excluded.
+smell_df = smell_df[smell_df['Smell_before_illness'] >= 50]
+taste_df = taste_df[taste_df['Taste_before_illness'] >= 50]
+
+# calculate recovery percentages
+smell_df['percentage_recovery'] = smell_df['Smell_current'] / smell_df['Smell_before_illness'] * 100
+taste_df['percentage_recovery'] = taste_df['Taste_current'] / taste_df['Taste_before_illness'] * 100
+
+# Individuals who do not have a follow upt rating that is at least 80% of their pre-illness function rating will be classified as chemosensory long-haulers.
+# Status is 0 for normal, 1 for long-haulers
+smell_df.loc[smell_df['percentage_recovery'] >= 80, 'Status'] = 0
+smell_df.loc[smell_df['percentage_recovery'] < 80, 'Status'] = 1
+taste_df.loc[taste_df['percentage_recovery'] >= 80, 'Status'] = 0
+taste_df.loc[taste_df['percentage_recovery'] < 80, 'Status'] = 1
+
+smell_df["Status"] = smell_df["Status"].astype('bool')
+taste_df["Status"] = taste_df["Status"].astype('bool')
+
+# study for smell
+smell_X = smell_df[['DurationInDays']]
+smell_y = smell_df['Smell_current'] - smell_df['Smell_before_illness']
+
+X_train, X_test, y_train, y_test = train_test_split(smell_X, smell_y, test_size=0.2)
+lm = LinearRegression()
+lm.fit(X_train, y_train)
+
+print('Coefficients: \n', lm.coef_)
+predictions = lm.predict(X_test)
+plt.scatter(y_test, predictions)
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
+
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
+time, survival_prob = kaplan_meier_estimator(smell_df["Status"], smell_df["DurationInDays"])
+plt.step(time, survival_prob, where="post")
+plt.ylabel("est. probability of survival $\hat{S}(t)$")
+plt.xlabel("time $t$")
+
+# study for taste
+taste_X = taste_df[['DurationInDays']]
+taste_y = taste_df['Taste_current'] - taste_df['Taste_before_illness']
+
+X_train, X_test, y_train, y_test = train_test_split(taste_X, taste_y, test_size=0.2)
+lm = LinearRegression()
+lm.fit(X_train, y_train)
+
+print('Coefficients: \n', lm.coef_)
+predictions = lm.predict(X_test)
+plt.scatter(y_test, predictions)
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
+
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
+time, survival_prob = kaplan_meier_estimator(taste_df["Status"], taste_df["DurationInDays"])
+plt.step(time, survival_prob, where="post")
+plt.ylabel("est. probability of survival $\hat{S}(t)$")
+plt.xlabel("time $t$")
